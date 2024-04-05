d5f815485c5cd136c1be02844874af3c0d3a1606
Evan Jelley <jelleyevan@gmail.com>
2024-04-05 13:16:15 -0600

    React week 1 work

--- React_Basics/firstapp/src/index.js
+++ React_Basics/firstapp/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Card from './components/Card';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App title="Welcome!"/>
+    <Card h2="Let's get started!" h3="This is a simple card component."/>
+    <Card h2="Another card!" h3="This is another card component."/>
+    <Card h2="This only has an h2 element."/>
   </React.StrictMode>
 );
 
